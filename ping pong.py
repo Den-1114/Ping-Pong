0464f4a8edb6fe780ea7c5f5eae4709a33c61677
DeniErMeh <denizkaya201215@gmail.com>
2023-09-14 15:21:53 +0300

    Updated version of ping pong

--- ping pong.py
+++ ping pong.py
@@ -1,4 +1,5 @@
 from pygame import *
+from random import *
 
 window = display.set_mode((1520, 780))
 clock = time.Clock()
@@ -37,13 +38,15 @@ class Platform(sprite.Sprite):
                 self.rect.y += self.speed
     
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed, speed_x, speed_y):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+        self.speed_x = speed_x
+        self.speed_y = speed_y
 
     def draw(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
@@ -51,10 +54,13 @@ class GameSprite(sprite.Sprite):
     def colliderect(self, rect):
         return self.rect.colliderect(rect)
 
+Balls = []
 
 Player1 = Platform(0, 255, 0, 1470, 760/2, 10, 100, 'a', 6)
 Player2 = Platform(255, 0, 0, 50, 760/2, 10, 100, 'b', 6)
-Ball = GameSprite('ball.png', 350, 250, 70, 70, 5)
+Ball = GameSprite('ball.png', 350, 250, 70, 70, 5, 3, 3)
+
+Balls.append(Ball)
 
 speed_x = 3
 speed_y = 3
@@ -62,8 +68,9 @@ speed_y = 3
 points_A = 0
 points_B = 0
 timer = 0
-FPS_timer = 60
+FPS_timer = 100
 timer = 120
+timer2 = 10
 finish = False
 font.init()
 font1 = font.Font(None, 30)
@@ -103,11 +110,11 @@ while game:
 
         FPS_timer -= 1
         if FPS_timer == 0:
-            FPS_timer = 60
-        if FPS_timer == 60:
+            FPS_timer = 100
+        if FPS_timer == 100:
+            timer2 -= 1
             timer -= 1
-
-        if timer == 0:
+        if timer == 10:
             if points_A > points_B:
                 A_Winner = font1.render(
                     'Player A is winner!!!', True, (0, 255, 0)
@@ -132,6 +139,13 @@ while game:
                 b = True
                 finish = True
 
+        if timer2 == 0:
+            speed_x += 0.4
+            speed_y += 0.4
+            Ball = GameSprite('ball.png', 350, 250, 70, 70, 5, speed_x, speed_y)
+            Balls.append(Ball)
+            timer2 = 10
+
         timer_lb = font1.render(
             str(timer) + 'seconds left', True, (255, 255, 255)
         )
@@ -148,35 +162,38 @@ while game:
         window.blit(score_A, (1330, 10))
         window.blit(score_B, (25, 10))
 
-        Ball.rect.x += speed_x
-        Ball.rect.y += speed_y
+        for Ball in Balls:
 
-        if Ball.colliderect(Player1.rect):
-            speed_x *= -1
+            Ball.rect.x += Ball.speed_x
+            Ball.rect.y += Ball.speed_y
 
-        if Ball.colliderect(Player2.rect):
-            speed_x *= -1
+            if Ball.colliderect(Player1.rect):
+                Ball.speed_x *= -1
 
-        if Ball.rect.y < 0:
-            speed_y *= -1
+            if Ball.colliderect(Player2.rect):
+                Ball.speed_x *= -1
+
+            if Ball.rect.y < 0:
+                Ball.speed_y *= -1
     
-        if Ball.rect.y > 730:
-            speed_y *= -1
+            if Ball.rect.y > 730:
+                Ball.speed_y *= -1
 
-        if Ball.rect.x > 1470:
-            speed_x *= -1
-            points_B += 1
+            if Ball.rect.x > 1470:
+                Ball.speed_x *= -1
+                points_B += 1
 
-        if Ball.rect.x < 0:
-            speed_x *= -1
-            points_A += 1
+            if Ball.rect.x < 0:
+                Ball.speed_x *= -1
+                points_A += 1
     
         Player1.draw_platform()
         Player1.update()
         Player2.draw_platform()
         Player2.update()
-        Ball.draw()
+        for Ball in Balls:
+            Ball.draw()
 
 
-    clock.tick(60)
+    clock.tick(100)
     display.update()
